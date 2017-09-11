534f1c647fe0ab0df4277941f36fa54015613391
EngineLin <linengine@gmail.com>
2017-09-11 10:41:09 +0800

    update 04

---
+++ scripts/all.js
@@ -0,0 +1,11 @@
+$(document).ready(function($) {
+	$("#bar").click(function(e) {
+		e.preventDefault();
+		$("#menuInside").toggle('fast', function() {
+		})
+	});
+	// $(window).resize(function(){
+	// 	var titleWidth = $('#title').width();
+	// 	$('#title').css('left', )
+	// })
+});
